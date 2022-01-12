65bff95203aef5e77a256b5bfbe318d6a233e2ad
naufalbasara <naufalbasara@outlook.com>
2022-01-12 10:32:15 +0700

    add project template views

--- project/views.py
+++ project/views.py
@@ -6,3 +6,18 @@ def index(request):
         'title': 'Projects'
     }
     return render(request, 'project/index.html', context)
+def benefits(request):
+    context = {
+        'img1': 'img/benefits/benefits.png',
+        'img2': 'img/benefits/benefits-dashboard.png',
+        'img3': 'img/benefits/benefits-view.png',
+        'title': 'BENEFITS Database App',
+        'duration': '7 - 12 days',
+        'timeFinished': '24 December 2021',
+        'description': 'Database web application use to ease the administration process of Tangerang regional forum.',
+        'explanation': 'BENEFITS database web application developed in order to ease the administration process of ITS Tangerang regional forum. In this website, users who mainly are BENEFITS administrator expected to maintain member data. Tech stack used in this project are Laravel (PHP Framework) and MySQL for the backend, while i used Bootstrap template for the display of the website.',
+        'goals': 'Project main objective is to ease the administration process of ITS Tangerang regional forum. In this website, users who mainly are BENEFITS administrator expected to maintain member data. For example, add, delete, and edit data of BENEFITS member. Users with certain user view (Treasury) may supervised cash flow with highlighted data in the web.',
+        'repository': 'https://github.com/naufalbasara/benefits-web-app',
+        'site': 'https://benefits-web-app.herokuapp.com/',
+    }
+    return render(request, 'project/template.html', context)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
     context = {
