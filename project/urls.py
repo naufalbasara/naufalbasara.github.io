f3d25c2a46fedf0fb9b3a386e44ceb8e1901fb2b
naufalbasara <naufalbasara@outlook.com>
2022-01-13 22:32:05 +0700

    fixing template views

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('benefits/', views.benefits)
+    path('benefits/', views.benefits),
+    path('js-mini/', views.jsmini)
 ]
--- project/views.py
+++ project/views.py
@@ -8,6 +8,7 @@ def index(request):
     return render(request, 'project/index.html', context)
 def benefits(request):
     context = {
+        'thumbnail': 'img/benefits/benefits.png',
         'img1': 'img/benefits/benefits.png',
         'img2': 'img/benefits/benefits-dashboard.png',
         'img3': 'img/benefits/benefits-view.png',
@@ -19,5 +20,21 @@ def benefits(request):
         'goals': 'Project main objective is to ease the administration process of ITS Tangerang regional forum. In this website, users who mainly are BENEFITS administrator expected to maintain member data. For example, add, delete, and edit data of BENEFITS member. Users with certain user view (Treasury) may supervised cash flow with highlighted data in the web.',
         'repository': 'https://github.com/naufalbasara/benefits-web-app',
         'site': 'https://benefits-web-app.herokuapp.com/',
+    }
+    return render(request, 'project/template.html', context)
+def jsmini(request):
+    context = {
+        'thumbnail': 'img/js-mini/thumbnail.png',
+        'img1': 'img/js-mini/basket-score-keeper.png',
+        'img2': 'img/js-mini/form-validation.png',
+        'img3': 'img/js-mini/form-validation2.png',
+        'title': 'JS Mini Project',
+        'duration': '1 - 3 days',
+        'timeFinished': 'October 2021',
+        'description': 'Simple mini Javascript projects made to fulfill college task.',
+        'explanation': 'BENEFITS database web application developed in order to ease the administration process of ITS Tangerang regional forum. In this website, users who mainly are BENEFITS administrator expected to maintain member data. Tech stack used in this project are Laravel (PHP Framework) and MySQL for the backend, while i used Bootstrap template for the display of the website.',
+        'goals': 'Project main objective is to ease the administration process of ITS Tangerang regional forum. In this website, users who mainly are BENEFITS administrator expected to maintain member data. For example, add, delete, and edit data of BENEFITS member. Users with certain user view (Treasury) may supervised cash flow with highlighted data in the web.',
+        'repository': 'https://github.com/naufalbasara/basketball-score-keeper.github.io',
+        'site': 'https://naufalbasara.github.io/basketball-score-keeper.github.io/',
     }
     return render(request, 'project/template.html', context)
